e798d9ffbb9df6a8957497087e546dcf18cc07a4
jamslaugh <gia.matrone@gmail.com>
2017-10-17 15:37:08 +0200

    Add files via upload
    This function allows to get the id field from scholar of every author listed in scholar.

---
+++ Scripts/Id function.R
@@ -0,0 +1,28 @@
+library(rvest)
+library(ggplot2)
+library(RCurl)
+library(XML)
+library(httr)
+library(stringr)
+library(dplyr)
+library(scholar)
+library(stringr)
+
+get_schol_id <- function(){
+  full_name = readline(prompt="Please, insert first name and Last Name of the scholar you are looking for: ")
+  url_search_tmp1 <- "https://scholar.google.it/citations?view_op=search_authors&mauthors=%s+%s%s"
+  url_search_tmp2 <- "&hl=it&oi=ao"
+  # From here we first unlist the scholar and then we add a + sign in order to get the search result
+  splitting <- strsplit(full_name, split = " ")
+  splitted <- unlist(splitting)
+  search_url <- sprintf(url_search_tmp1,splitted[1],splitted[2],url_search_tmp2)
+  srch_page <- GET(search_url, handle=getOption("scholar_handle")) %>% htmlParse()
+  links<-xpathSApply(srch_page,path = "//a",xmlGetAttr,"href")
+  id_tmp <- str_extract(links,"[A-Za-z0-9]{12}")
+  id_tmp <- id_tmp[!is.na(id_tmp)]
+  if(length(id_tmp) > 1){
+    return(id_tmp[1])
+  }else{
+    return(id_tmp)
+  }
+}
